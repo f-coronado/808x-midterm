f8d52de7404b3dba55af2f7206f52bb40bf1472d
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-23 09:59:30 -0400

    Task 2.1 Jacobian test stub: Created Jacobian test in test/test.cpp to check if the create_Jx method works as expected.

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <transform.hpp>
-#include <cmath>
+#include <jacobian.hpp>
 
 TEST(TransformTest, GetTransformFn) {
   double q = .5*M_PI;
@@ -15,8 +15,24 @@ TEST(TransformTest, GetTransformFn) {
                 sin(q), cos(q)*cos(alpha), -cos(q)*sin(alpha), a*sin(q),
                 0, sin(alpha), cos(alpha), d,
                 0, 0, 0, 1;
-  std::cout << "\nexpectedT:" << expectedT << std::endl;
-  std::cout << "\nT:" << T << std::endl;
-
   EXPECT_TRUE(T.isApprox(expectedT, 1e-4));
   };
+
+TEST(JacobianTest, GetJx){
+
+  jacobian jacobian;
+  Eigen::Matrix<double, 3, 1> J_v;
+  J_v << 1,
+         0,
+         1;
+  Eigen::Matrix<double, 3, 1> J_w;
+  J_w << 2,
+         0,
+         2;
+  Eigen::Matrix<double, 3, 6> expectedJ;
+  expectedJ << J_v, J_v, J_v, J_w, J_w, J_w;
+
+  Eigen::Matrix<double, 3, 6> Jx = jacobian.create_Jx(J_v, J_v, J_v, J_w, J_w, J_w);
+
+  EXPECT_TRUE(Jx.isApprox(expectedJ, 1e-4));
+};
