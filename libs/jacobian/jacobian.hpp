a7aeb5ee446c3c6047dacb98616c27c863bf4058
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-29 02:15:01 -0400

    Task 4.0 Develop jacobian class: Added velocity_IK function implementation to src.cpp and definition in jacobian.hpp

--- libs/jacobian/jacobian.hpp
+++ libs/jacobian/jacobian.hpp
@@ -1,5 +1,10 @@
+#pragma once
 #include <eigen3/Eigen/Core>
 
+/**
+ * @brief jacobian class contains members used for creating jacobian matrices
+ * controller.
+ */
 class jacobian {
     private:
         Eigen::Matrix<double, 3, 1> J_v1;
@@ -8,12 +13,35 @@ class jacobian {
         Eigen::Matrix<double, 3, 1> J_v4;
         Eigen::Matrix<double, 3, 1> J_v5;
         Eigen::Matrix<double, 3, 1> J_v6;
+        Eigen::Matrix<double, 3, 1> Z_0;
+
 
     public:
+        
+        /**
+        * @brief Method that constructs a 3x6 vector once passed 6 3x1 vectors.
+        * @param J_v1 the partial derivative of the tranformation from the base to end effector psotion wrt joint 1
+        * @param J_v2 the partial derivative of the tranformation from the base to end effector psotion wrt joint 2
+        * @param J_v3 the partial derivative of the tranformation from the base to end effector psotion wrt joint 3
+        * @param J_v4 the partial derivative of the tranformation from the base to end effector psotion wrt joint 4
+        * @param J_v5 the partial derivative of the tranformation from the base to end effector psotion wrt joint 5
+        * @param J_v6 the partial derivative of the tranformation from the base to end effector psotion wrt joint 6       
+        */
         Eigen::Matrix<double, 3, 6> create_Jx(Eigen::Matrix<double, 3, 1> J_v1,
                                   Eigen::Matrix<double, 3, 1> J_v2,
                                   Eigen::Matrix<double, 3, 1> J_v3,
                                   Eigen::Matrix<double, 3, 1> J_v4,
                                   Eigen::Matrix<double, 3, 1> J_v5,
                                   Eigen::Matrix<double, 3, 1> J_v6);
+
+        /**
+        * @brief Method returns the velocity inverse kinematics
+        * @param q_joint the joint angles of the manipulator
+        * @param x_dot the velocity of the end effector
+        */
+        Eigen::Matrix<double, 6, 1> velocity_IK(Eigen::Matrix<double, 1, 6> q_joint,
+                                                Eigen::Matrix<double, 6, 1> x_dot
+                                                Eigen::Matrix<double, 6, 6> J
+                                                );
+
 };
--- libs/jacobian/src.cpp
+++ libs/jacobian/src.cpp
@@ -12,3 +12,11 @@ Eigen::Matrix<double, 3, 6> jacobian::create_Jx(Eigen::Matrix<double, 3, 1> J_v1
     Jx << J_v1, J_v2, J_v3, J_v4, J_v5, J_v6;
     return Jx;
     }
+
+Eigen::Matrix<double, 6, 1> velocity_IK(Eigen::Matrix<double, 1, 6> q_joint,
+                                        Eigen::Matrix<double, 6, 1> x_dot,
+                                        Eigen::Matrix<double, 6, 1> J
+                                        ){
+    Eigen::Matrix<double, 6, 1> q_dot = J.inverse() * x_dot;
+    return q_dot;
+    }       
