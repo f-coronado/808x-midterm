9eb6efaca8a9c98e04041b4bba7e627fdb2def4b
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-29 17:30:45 -0400

    Task 4.5 develop demo: updated CMakeLists.txt to add transform and joint_angles library. In main.cpp I added the transformation matrix from base to end effector

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,44 @@
-#include "lib.hpp"
-#include "lib1.hpp"
-#include "lib2.hpp"
+#include <eigen3/Eigen/Core>
+#include <iostream>
+#include <jacobian.hpp>
+#include <symengine/symbol.h>
+using SymEngine::Expression;
+#include <symengine/derivative.h>
+#include <transform.hpp>
+#include <joint_angles.hpp>
 
 int main() {
-  dummy();
-  my_function1(30);
-  my_function2(30.3);
-  return 0;
+  std::cout << "Demo..." << std::endl;
+  jacobian jacobian;
+  transform transform;
+  // joint_angles joint_angles;
+
+  Eigen::Matrix<double, 1, 7> q_joint;
+  q_joint << 0, 0, 3.14/2, 0, 3.14, 0, 1;
+
+  Eigen::Matrix<double, 1, 6> x_dot;
+  x_dot << 0, -4*M_PI*sin(M_PI/2), 4*M_PI*cos(M_PI/2), 0, 0, 0;
+  std::cout << "\nUsing x_dot: " << x_dot << "\nand q_joint: " << q_joint << "\n";
+
+  Eigen::Matrix4d T_01 = transform.get_transform(q_joint(0,0), transform.d1, 0, M_PI/2);
+  Eigen::Matrix4d T_12 = transform.get_transform(q_joint(0,1), 0, 0, -M_PI/2);
+  Eigen::Matrix4d T_23 = transform.get_transform(0, transform.d3, transform.a3, -M_PI/2);
+  Eigen::Matrix4d T_34 = transform.get_transform(q_joint(0,3), 0, -transform.a3, M_PI/2);
+  Eigen::Matrix4d T_45 = transform.get_transform(q_joint(0,4), transform.d5, 0, M_PI/2);
+  Eigen::Matrix4d T_56 = transform.get_transform(q_joint(0,5), 0, transform.a3, -M_PI/2);
+  Eigen::Matrix4d T_6n = transform.get_transform(q_joint(0,6), -transform.d7, 0, 0);
+
+  Eigen::Matrix4d T_02 = T_01 * T_12;
+  Eigen::Matrix4d T_03 = T_02 *T_23;
+  Eigen::Matrix4d T_04 = T_02 * T_23 * T_34;
+  Eigen::Matrix4d T_05 = T_04 * T_45;
+  Eigen::Matrix4d T_06 = T_05 * T_56;
+  Eigen::Matrix4d T_0n = T_06 * T_6n;
+  std::cout << "Transformation from base to end effector is: \n" << T_0n << std::endl;
+
+  Eigen::Matrix <double, 4, 1> x_p;
+  x_p << T_0n(0, 3), T_0n(1, 3), T_0n(2, 3), T_0n(3, 3);
+  std::cout << "x_p:\n" << x_p << std::endl;
+
+
 }
