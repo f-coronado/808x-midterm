862cb813112262458997185adaa278a57551ff41
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-30 12:57:18 -0400

    Task 4.2 Document classes: Added doxygen documentation to private members and information about this class such as the file, author, copyright and date.

--- libs/jacobian/jacobian.hpp
+++ libs/jacobian/jacobian.hpp
@@ -1,47 +1,54 @@
 #pragma once
 #include <eigen3/Eigen/Core>
+#include <symengine/subs.h>
+#include <symengine/symbol.h>
 
 /**
- * @brief jacobian class contains members used for creating jacobian matrices
- * controller.
+ * @file jacobian.hpp
+ * @brief Jacobian class for generating Jacobian matrices in a robotic arm controller.
+ * @author Fabrizzio Coronado
+ * @date 2023
+ * @copyright Copyright (c) 2023 by Fabrizzio Coronado.
  */
 class jacobian {
-    private:
-        Eigen::Matrix<double, 3, 1> J_v1;
-        Eigen::Matrix<double, 3, 1> J_v2;
-        Eigen::Matrix<double, 3, 1> J_v3;
-        Eigen::Matrix<double, 3, 1> J_v4;
-        Eigen::Matrix<double, 3, 1> J_v5;
-        Eigen::Matrix<double, 3, 1> J_v6;
-        Eigen::Matrix<double, 3, 1> Z_0;
+private:
+    Eigen::Matrix<double, 3, 1> J_v1; /**< Partial derivative of the transformation from base to end effector position with respect to joint 1 */
+    Eigen::Matrix<double, 3, 1> J_v2; /**< Partial derivative of the transformation from base to end effector position with respect to joint 2 */
+    Eigen::Matrix<double, 3, 1> J_v3; /**< Partial derivative of the transformation from base to end effector position with respect to joint 3 */
+    Eigen::Matrix<double, 3, 1> J_v4; /**< Partial derivative of the transformation from base to end effector position with respect to joint 4 */
+    Eigen::Matrix<double, 3, 1> J_v5; /**< Partial derivative of the transformation from base to end effector position with respect to joint 5 */
+    Eigen::Matrix<double, 3, 1> J_v6; /**< Partial derivative of the transformation from base to end effector position with respect to joint 6 */
+    Eigen::Matrix<double, 3, 1> Z_0; /**< A 3x1 matrix (column vector) */
+    Eigen::Matrix<double, 6, 6> J; /**< The Jacobian matrix (6x6) */
+    Eigen::Matrix<double, 6, 6> J_inv; /**< The inverse of the Jacobian matrix (6x6) */
+    Eigen::Matrix<double, 6, 1> q_dot; /**< A 6x1 matrix representing joint velocities */
 
+public:
+    /**
+     * @brief Constructs a 3x6 Jacobian matrix using six 3x1 vectors.
+     * @param J_v1 Partial derivative of transformation from base to end effector position with respect to joint 1.
+     * @param J_v2 Partial derivative of transformation from base to end effector position with respect to joint 2.
+     * @param J_v3 Partial derivative of transformation from base to end effector position with respect to joint 3.
+     * @param J_v4 Partial derivative of transformation from base to end effector position with respect to joint 4.
+     * @param J_v5 Partial derivative of transformation from base to end effector position with respect to joint 5.
+     * @param J_v6 Partial derivative of transformation from base to end effector position with respect to joint 6.
+     * @return A 3x6 Jacobian matrix.
+     */
+    Eigen::Matrix<double, 3, 6> create_Jx(Eigen::Matrix<double, 3, 1> J_v1,
+                                          Eigen::Matrix<double, 3, 1> J_v2,
+                                          Eigen::Matrix<double, 3, 1> J_v3,
+                                          Eigen::Matrix<double, 3, 1> J_v4,
+                                          Eigen::Matrix<double, 3, 1> J_v5,
+                                          Eigen::Matrix<double, 3, 1> J_v6);
 
-    public:
-        
-        /**
-        * @brief Method that constructs a 3x6 vector once passed 6 3x1 vectors.
-        * @param J_v1 the partial derivative of the tranformation from the base to end effector psotion wrt joint 1
-        * @param J_v2 the partial derivative of the tranformation from the base to end effector psotion wrt joint 2
-        * @param J_v3 the partial derivative of the tranformation from the base to end effector psotion wrt joint 3
-        * @param J_v4 the partial derivative of the tranformation from the base to end effector psotion wrt joint 4
-        * @param J_v5 the partial derivative of the tranformation from the base to end effector psotion wrt joint 5
-        * @param J_v6 the partial derivative of the tranformation from the base to end effector psotion wrt joint 6       
-        */
-        Eigen::Matrix<double, 3, 6> create_Jx(Eigen::Matrix<double, 3, 1> J_v1,
-                                  Eigen::Matrix<double, 3, 1> J_v2,
-                                  Eigen::Matrix<double, 3, 1> J_v3,
-                                  Eigen::Matrix<double, 3, 1> J_v4,
-                                  Eigen::Matrix<double, 3, 1> J_v5,
-                                  Eigen::Matrix<double, 3, 1> J_v6);
-
-        /**
-        * @brief Method returns the velocity inverse kinematics
-        * @param q_joint the joint angles of the manipulator
-        * @param x_dot the velocity of the end effector
-        */
-        Eigen::Matrix<double, 6, 1> velocity_IK(Eigen::Matrix<double, 1, 6> q_joint,
-                                                Eigen::Matrix<double, 6, 1> x_dot
-                                                Eigen::Matrix<double, 6, 6> J
-                                                );
-
+    /**
+     * @brief Computes the velocity inverse kinematics.
+     * @param q_joint The joint angles of the manipulator (6x1 matrix).
+     * @param x_dot The velocity of the end effector (6x1 matrix).
+     * @param J The Jacobian matrix (6x6) used in the computation.
+     * @return A 6x1 matrix representing the joint velocities.
+     */
+    Eigen::Matrix<double, 6, 1> velocity_IK(Eigen::Matrix<double, 1, 6> q_joint,
+                                            Eigen::Matrix<double, 6, 1> x_dot,
+                                            Eigen::Matrix<double, 6, 6> J);
 };
