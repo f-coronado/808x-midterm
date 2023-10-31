4176c585d70899367f4c8e11acdd4620a438be4d
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-31 18:21:40 -0400

    Task 5.2 & 5.4: Completed jacobian and joint angles tests successfully with new updated classes

--- test/test.cpp
+++ test/test.cpp
@@ -3,54 +3,33 @@
 #include <jacobian.hpp>
 #include <joint_angles.hpp>
 
-TEST(TransformTest, GetTransformFn) {
-  double q = .5*M_PI;
-  double d = 9;
-  double a = 5;
-  double alpha = 0;
-  transform transform;
-  Eigen::Matrix4d T = transform.get_transform(q, d, a, alpha);
-
-  Eigen::Matrix4d expectedT;
-    expectedT << cos(q), -sin(q)*cos(alpha), sin(q)*sin(alpha), a*cos(q),
-                sin(q), cos(q)*cos(alpha), -cos(q)*sin(alpha), a*sin(q),
-                0, sin(alpha), cos(alpha), d,
-                0, 0, 0, 1;
-  EXPECT_TRUE(T.isApprox(expectedT, 1e-4));
-  };
-
-TEST(JacobianTest, GetJx){
+TEST(jacobianTest, getJ){
 
   jacobian jacobian;
-  Eigen::Matrix<double, 3, 1> J_v;
-  J_v << 1,
-         0,
-         1;
-  Eigen::Matrix<double, 3, 1> J_w;
-  J_w << 2,
-         0,
-         2;
-  Eigen::Matrix<double, 3, 6> expectedJ;
-  expectedJ << J_v, J_v, J_v, J_w, J_w, J_w;
+  Eigen::Matrix<double, 6, 6> expectedJ;
+  expectedJ << -8.8, -49.3, 0, -8.8,  1.26751e-15, 0,
+                0, -10.9, 0, 5.38845e-16, 20.7, 0,
+                0, 0, 8.8, 0, 8.8, 0,
+                0, 0, -0, -0, 1, 0,
+                0, -1, -0, -0, -6.12323e-17, -0,
+                1, 0, 1, 1, 0, 1;
 
-  Eigen::Matrix<double, 3, 6> Jx = jacobian.create_Jx(J_v, J_v, J_v, J_w, J_w, J_w);
+  Eigen::Matrix<double, 6, 6> J = jacobian.j_matrix(0, 0, M_PI/2, 0, M_PI/2, 0);
 
-  EXPECT_TRUE(Jx.isApprox(expectedJ, 1e-4));
+  EXPECT_TRUE(J.isApprox(expectedJ, 1e-4));
 };
 
-TEST(JointAnglesTest, ComputeJointAngles) {
-    YourRobotClass robot; // Create an instance of your robot class
+TEST(jointAnglesTest, getQdot){
 
-    // Define the expected joint angles (replace with actual values)
-    std::vector<double> expectedJointAngles = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+  joint_angles angles;
+  jacobian jacobian;
+  
+  Eigen::Matrix<double, 6, 1> x_dot;
+  x_dot << 0, -4*M_PI*sin(M_PI/2), 4*M_PI*cos(M_PI/2), 0, 0, 0;
+  Eigen::Matrix<double, 1, 6> initial_angles(6);
+  initial_angles << double(0), double(0), double(M_PI/2), double(0), double(M_PI/2), double(0);
+  Eigen::Matrix<double, 6, 6> J = jacobian.j_matrix(0, 0, M_PI/2, 0, M_PI/2, 0);
 
-    // Call the function to compute joint angles
-    std::vector<double> computedJointAngles = robot.computeJointAngles();
+  Eigen::Matrix<double, 6, 1> q_dot = angles.velocity_IK(initial_angles, x_dot, J);
 
-    // Use EXPECT or ASSERT to check if the computed joint angles match the expected values
-    ASSERT_EQ(computedJointAngles.size(), expectedJointAngles.size());
-
-    for (size_t i = 0; i < computedJointAngles.size(); ++i) {
-        EXPECT_NEAR(computedJointAngles[i], expectedJointAngles[i], 1e-4); // Adjust the tolerance (1e-4) as needed
-    }
-};
+}
