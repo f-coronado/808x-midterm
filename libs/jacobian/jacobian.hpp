0799cbe80319b2457ed36b8d030a9d1df51bbabb
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-31 16:51:01 -0400

    Task 4.9 Develop Jacobian class: removed prior functions, created new hard coded jacobian function inside jacobian.hpp and src.cpp for franka emika panda arm. Function is named j_matrix

--- libs/jacobian/jacobian.hpp
+++ libs/jacobian/jacobian.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <eigen3/Eigen/Core>
-#include <symengine/subs.h>
-#include <symengine/symbol.h>
+#include <eigen3/Eigen/LU>
+#include <eigen3/Eigen/Dense>
 
 /**
  * @file jacobian.hpp
@@ -24,31 +24,17 @@ private:
     Eigen::Matrix<double, 6, 1> q_dot; /**< A 6x1 matrix representing joint velocities */
 
 public:
-    /**
-     * @brief Constructs a 3x6 Jacobian matrix using six 3x1 vectors.
-     * @param J_v1 Partial derivative of transformation from base to end effector position with respect to joint 1.
-     * @param J_v2 Partial derivative of transformation from base to end effector position with respect to joint 2.
-     * @param J_v3 Partial derivative of transformation from base to end effector position with respect to joint 3.
-     * @param J_v4 Partial derivative of transformation from base to end effector position with respect to joint 4.
-     * @param J_v5 Partial derivative of transformation from base to end effector position with respect to joint 5.
-     * @param J_v6 Partial derivative of transformation from base to end effector position with respect to joint 6.
-     * @return A 3x6 Jacobian matrix.
-     */
-    Eigen::Matrix<double, 3, 6> create_Jx(Eigen::Matrix<double, 3, 1> J_v1,
-                                          Eigen::Matrix<double, 3, 1> J_v2,
-                                          Eigen::Matrix<double, 3, 1> J_v3,
-                                          Eigen::Matrix<double, 3, 1> J_v4,
-                                          Eigen::Matrix<double, 3, 1> J_v5,
-                                          Eigen::Matrix<double, 3, 1> J_v6);
+    double q1;
+    double q2;
+    double q3;
+    double q4;
+    double q5;
+    double q6;
 
-    /**
-     * @brief Computes the velocity inverse kinematics.
-     * @param q_joint The joint angles of the manipulator (6x1 matrix).
-     * @param x_dot The velocity of the end effector (6x1 matrix).
-     * @param J The Jacobian matrix (6x6) used in the computation.
-     * @return A 6x1 matrix representing the joint velocities.
-     */
-    Eigen::Matrix<double, 6, 1> velocity_IK(Eigen::Matrix<double, 1, 6> q_joint,
-                                            Eigen::Matrix<double, 6, 1> x_dot,
-                                            Eigen::Matrix<double, 6, 6> J);
+    Eigen::Matrix <double, 6, 6> j_matrix(double q1, 
+                                        double q2, 
+                                        double q3, 
+                                        double q4, 
+                                        double q5, 
+                                        double q6);
 };
--- libs/jacobian/src.cpp
+++ libs/jacobian/src.cpp
@@ -1,22 +1,86 @@
+#include "jacobian.hpp"
 #include <eigen3/Eigen/Core>
-#include <jacobian.hpp>
+#include <eigen3/Eigen/Dense>
+
+/**
+ * @file jacobian.hpp
+ * @brief Jacobian class for generating Jacobian matrices in a robotic arm controller.
+ * @author Fabrizzio Coronado
+ * @date 2023
+ * @copyright Copyright (c) 2023 by Fabrizzio Coronado.
+ */
+
+Eigen::Matrix <double, 6, 6> jacobian::j_matrix(double q1, 
+                                            double q2, 
+                                            double q3, 
+                                            double q4, 
+                                            double q5, 
+                                            double q6){
+    Eigen::Matrix <double, 6, 6> J;
+    J(0, 0)= 31.6*sin(q1)*sin(q2) + 8.8*sin(q1)*sin(q6)*sin(q2-q4) - \
+    20.7*sin(q1)*sin(q6)*cos(q5)*cos(q2-q4)-20.7*sin(q1)*sin(q2-q4)*cos(q6) + \
+    38.4*sin(q1)*sin(q2-q4) - 8.8*sin(q1)*cos(q2) - 8.8*sin(q1)*cos(q5)*cos(q6)*cos(q2-q4) - \
+    20.7*sin(q5)*sin(q6)*cos(q1) - 8.8*sin(q5)*cos(q1)*cos(q6);
+    J(1, 0) = -20.7*sin(q1)*sin(q5)*sin(q6) - 8.8*sin(q1)*sin(q5)*cos(q6) - \
+    31.6*sin(q2)*cos(q1) - 8.8*sin(q6)*sin(q2-q4)*cos(q1) + \
+    20.7*sin(q6)*cos(q1)*cos(q5)*cos(q2-q4)*cos(q1)*cos(q6) - 38.4*sin(q2-q4)*cos(q1) + \
+    8.8*cos(q1)*cos(q2) + 8.8*cos(q1)*cos(q5)*cos(q6)*cos(q2-q4) - 8.8*cos(q1)*cos(q2-q4);
+    J(2, 0) = 0;
+    J(3, 0) = 0;
+    J(4, 0) = 0;
+    J(5, 0) = 1;
+
+    J(0, 1) = (-8.8*sin(q2) - 20.7*sin(q6)*sin(q2 - q4)*cos(q4) - 8.8*sin(q6)*cos(q2 - q4) -\
+    8.8*sin(q2 - q4)*cos(q4)*cos(q6) + 8.8*sin(q2 - q4) - 31.6*cos(q2) + \
+    20.7*cos(q6)*cos(q2 - q4) - 38.4*cos(q2 - q4))*cos(q1);
+    J(1, 1) = -8.8*sin(q2) - 20.7*sin(q6)*sin(q2 - q4)*cos(q5) - 8.8*sin(q6)*cos(q2 - q4) - \
+    8.8*sin(q2 - q4)*cos(q5)*cos(q6) + 8.8*sin(q2 - q4) - 31.6*cos(q2) + \
+    20.7*cos(q6)*cos(q2 - q4) - 38.4*cos(q2 - q4)*sin(q1);
+    J(2, 1) = -31.6*sin(q2) - 8.8*sin(q6)*sin(q2 - q4) + 20.7*sin(q6)*cos(q5)*cos(q2 - q4) + \
+    20.7*sin(q2 - q4)*cos(q6) - 38.4*sin(q2 - q4) + 8.8*cos(q2) + \
+    8.8*cos(q5)*cos(q6)*cos(q2 - q4) - 8.8*cos(q2 - q4);
+    J(3, 1) = sin(q1);
+    J(4, 1) = -cos(q1);
+    J(5, 1) = 0;
+
+    J(0, 2) = (20.7*sin(q6)*sin(q2 - q4)*cos(q5) + 8.8*sin(q6)*cos(q2 - q4) + \
+    8.8*sin(q2 - q4)*cos(q5)*cos(q6) - 8.8*sin(q2 - q4) - 20.7*cos(q6)*cos(q2 - q4) + \
+    38.4*cos(q2 - q4))*sin(q1);
+    J(1, 2) = (20.7*sin(q6)*sin(q2 - q4)*cos(q5) + 8.8*sin(q6)*cos(q2 - q4) + \
+    8.8*sin(q2 - q4)*cos(q5)*cos(q6) - 8.8*sin(q2 - q4) - 20.7*cos(q6)*cos(q2 - q4) + \
+    38.4*cos(q2 - q4))*sin(q1);
+    J(2, 2) = 8.8*sin(q6)*sin(q2 - q4) - 20.7*sin(q6)*cos(q5)*cos(q2 - q4) - \
+    20.7*sin(q2 - q4)*cos(q6) + 38.4*sin(q2 - q4) - 8.8*cos(q5)*cos(q6)*cos(q2 - q4) + \
+    8.8*cos(q2 - q4);
+    J(3, 2) = -sin(q2)*cos(q1);
+    J(4, 2) = -sin(q1)*sin(q2);
+    J(5, 2) = cos(q2);
 
 
-Eigen::Matrix<double, 3, 6> jacobian::create_Jx(Eigen::Matrix<double, 3, 1> J_v1,
-                           Eigen::Matrix<double, 3, 1> J_v2,
-                           Eigen::Matrix<double, 3, 1> J_v3,
-                           Eigen::Matrix<double, 3, 1> J_v4,
-                           Eigen::Matrix<double, 3, 1> J_v5,
-                           Eigen::Matrix<double, 3, 1> J_v6){
-    Eigen::Matrix<double, 3, 6> Jx;
-    Jx << J_v1, J_v2, J_v3, J_v4, J_v5, J_v6;
-    return Jx;
-    }
+    J(0, 3) = -(sin(q1)*cos(q5) + sin(q5)*cos(q1)*cos(q2 - q4))*(20.7*sin(q6) + 8.8*cos(q6));
+    J(1, 3) = -(sin(q1)*sin(q5)*cos(q2 - q4) - cos(q1)*cos(q5))*(20.7*sin(q6) + 8.8*cos(q6));
+    J(2, 3) = -(20.7*sin(q6) + 8.8*cos(q6))*sin(q5)*sin(q2 - q4);
+    J(3, 3) = -sin(q2 - q4)*cos(q1);
+    J(4, 3) = -sin(q1)*sin(q2 - q4);
+    J(5, 3) = cos(q2 - q4);
 
-Eigen::Matrix<double, 6, 1> velocity_IK(Eigen::Matrix<double, 1, 6> q_joint,
-                                        Eigen::Matrix<double, 6, 1> x_dot,
-                                        Eigen::Matrix<double, 6, 1> J
-                                        ){
-    Eigen::Matrix<double, 6, 1> q_dot = J.inverse() * x_dot;
-    return q_dot;
-    }       
+    J(0, 4) = 8.8*(sin(q1)*sin(q5) - cos(q1)*cos(q5)*cos(q2 - q4))*sin(q6) - \
+    20.7*(sin(q1)*sin(q5) - cos(q1)*cos(q5)*cos(q2 - q4))*cos(q6) - \
+    20.7*sin(q6)*sin(q2 - q4)*cos(q1) - 8.8*sin(q2 - q4)*cos(q1)*cos(q6);
+    J(1, 4) = -8.8*(sin(q1)*cos(q5)*cos(q2 - q4) + sin(q5)*cos(q1))*sin(q6) + \
+    20.7*(sin(q1)*cos(q5)*cos(q2 - q4) + sin(q5)*cos(q1))*cos(q6) - \
+    20.7*sin(q1)*sin(q6)*sin(q2 - q4) - 8.8*sin(q1)*sin(q2 - q4)*cos(q6);
+    J(2, 4) = -8.8*sin(q6)*sin(q2 - q4)*cos(q5) + 20.7*sin(q6)*cos(q2 - q4) + \
+    20.7*sin(q2 - q4)*cos(q5)*cos(q6) + 8.8*cos(q6)*cos(q2 - q4);
+    J(3, 4) = sin(q1)*cos(q5) + sin(q5)*cos(q1)*cos(q2 - q4);
+    J(4, 4) = sin(q1)*sin(q5)*cos(q2 - q4) - cos(q1)*cos(q5);
+    J(5, 4) = sin(q5)*sin(q2 - q4);
+
+    J(0, 5) = 0;
+    J(1, 5) = 0;
+    J(2, 5) = 0;
+    J(3, 5) = (sin(q1)*sin(q5) - cos(q1)*cos(q5)*cos(q2 - q4))*sin(q6) - sin(q2 - q4)*cos(q1)*cos(q6);
+    J(4, 5) = -(sin(q1)*cos(q5)*cos(q2 - q4) + sin(q5)*cos(q1))*sin(q6) - sin(q1)*sin(q2 - q4)*cos(q6);
+    J(5, 5) = -sin(q6)*sin(q2 - q4)*cos(q5) + cos(q6)*cos(q2 - q4);
+    return J;
+}
