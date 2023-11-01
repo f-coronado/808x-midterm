99dbb99c72bcbee8573fec3f2667e04f78c851ff
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-31 21:22:45 -0400

    Task 6.1 Implement solitary testing: removed jacobian function from jointAnglesTest, instead i used the expected jacobian

--- test/test.cpp
+++ test/test.cpp
@@ -17,7 +17,6 @@ TEST(jacobianTest, getJ) {
 
 TEST(jointAnglesTest, getQdot) {
   joint_angles angles;
-  jacobian jacobian;
 
   Eigen::Matrix<double, 6, 1> x_dot;
   x_dot << 0, -4 * M_PI * sin(M_PI / 2), 4 * M_PI * cos(M_PI / 2), 0, 0, 0;
@@ -25,8 +24,10 @@ TEST(jointAnglesTest, getQdot) {
   initial_angles << static_cast<double>(0), static_cast<double>(0), \
   static_cast<double>(M_PI / 2), static_cast<double>(0), \
   static_cast<double>(M_PI / 2), static_cast<double>(0);
-  Eigen::Matrix<double, 6, 6> J =
-      jacobian.j_matrix(0, 0, M_PI / 2, 0, M_PI / 2, 0);
+  Eigen::Matrix<double, 6, 6> J;
+  J << -8.8, -49.3, 0, -8.8, 1.26751e-15, 0, 0, -10.9, 0, 5.38845e-16,
+      20.7, 0, 0, 0, 8.8, 0, 8.8, 0, 0, 0, -0, -0, 1, 0, 0, -1, -0, -0,
+      -6.12323e-17, -0, 1, 0, 1, 1, 0, 1;
 
   Eigen::Matrix<double, 6, 1> q_dot = \
   angles.velocity_IK(initial_angles, x_dot, J);
